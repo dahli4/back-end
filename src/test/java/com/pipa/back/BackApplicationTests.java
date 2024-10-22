d87e70d46d24c3e1c7e0dc52341cc751efdc7e9b
동영 <57888678+dahli4@users.noreply.github.com>
2024-10-22 17:01:22 +0900

    Initial commit

---
+++ src/test/java/com/pipa/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipa.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
